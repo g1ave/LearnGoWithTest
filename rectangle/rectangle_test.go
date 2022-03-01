b125018c1ab270dc560071e5b29d2e3500d10ae7
g1ave <qw112657@gmail.com>
2022-03-01 11:11:22 +0800

    第一次重构

--- rectangle/rectangle_test.go
+++ rectangle/rectangle_test.go
@@ -11,13 +11,16 @@ func TestPerimeter(t *testing.T) {
 	}
 
 	t.Run("caculate the perimeter of rectangle", func(t *testing.T) {
-		got := Perimeter(10.0, 10.0)
+		rectangle := Rectangle{10.0, 10.0}
+
+		got := Perimeter(rectangle)
 		want := 40.0
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("caculate the area of the rectangle", func(t *testing.T) {
-		got := Area(12.0, 6.0)
+		rectangle := Rectangle{12.0, 6.0}
+		got := Area(rectangle)
 		want := 72.0
 		assertCorrectMessage(t, got, want)
 	})
