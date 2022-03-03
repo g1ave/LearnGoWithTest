280e1d435e8a6a62860788f48d8f761fe04455d9
g1ave <qw112657@gmail.com>
2022-03-03 22:35:47 +0800

    Feature: return an error when no word found

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,7 +1,15 @@
 package dictionary
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", errors.New("couldn't find the word you are looking for")
+	}
+	return definition, nil
+
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -3,12 +3,27 @@ package dictionary
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is a test"}
 
-	got := dictionary.Search("test")
-	want := "this is a test"
+	t.Run("word exists", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is a test"}
 
-	assertStrings(t, got, want)
+		got, _ := dictionary.Search("test")
+		want := "this is a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		dictionary := Dictionary{}
+
+		got, err := dictionary.Search("unknown")
+
+		if err == nil {
+			t.Fatal("expected an error but didn't get one")
+		}
+
+		assertStrings(t, got, "")
+	})
 }
 
 func assertStrings(t testing.TB, got string, want string) {
