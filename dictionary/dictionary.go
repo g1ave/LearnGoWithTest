72c5f9ce3ef8e27ec2434f0845fc902e7b8ab84b
g1ave <qw112657@gmail.com>
2022-03-06 17:14:32 +0800

    Feature: update & delete

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -3,8 +3,9 @@ package dictionary
 type Dictionary map[string]string
 
 const (
-	NotFoundError    = DictionaryErr("counld't find the word you are looking for")
-	WordExistedError = DictionaryErr("cannot add word because it already exists")
+	NotFoundError         = DictionaryErr("counld't find the word you are looking for")
+	WordExistedError      = DictionaryErr("cannot add word because it already exists")
+	WordDoesNotExistError = DictionaryErr("can't update the word because it doesnot exist")
 )
 
 type DictionaryErr string
@@ -29,10 +30,25 @@ func (d Dictionary) Add(word, definition string) error {
 	switch err {
 	case NotFoundError:
 		d[word] = definition
-		return nil
 	case nil:
 		return WordExistedError
 	default:
 		return err
 	}
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case NotFoundError:
+		return WordDoesNotExistError
+	case nil:
+		d[word] = newDefinition
+	default:
+		return err
+	}
+
+	return nil
 }
