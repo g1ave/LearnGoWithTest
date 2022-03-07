7d3ab312f05bd9374fbb682cecbabac85cdaabc0
g1ave <qw112657@gmail.com>
2022-03-07 10:52:13 +0800

    Feature: implement ConfigurableSleeper

--- countdown/main.go
+++ countdown/main.go
@@ -10,17 +10,30 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
 
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	configurableSleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, configurableSleeper)
 }
