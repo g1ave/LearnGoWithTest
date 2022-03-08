b30f2f4b6a27bb298045c5513b3b030064e2e31c
g1ave <qw112657@gmail.com>
2022-03-08 16:48:46 +0800

    Test: Mock http server

--- racer/webRacer_test.go
+++ racer/webRacer_test.go
@@ -1,10 +1,23 @@
 package webRacer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "https://www.facebook.com"
-	fastURL := "https://www.baidu.com"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastSever := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	slowURL := slowServer.URL
+	fastURL := fastSever.URL
 
 	got := Racer(slowURL, fastURL)
 	want := fastURL
@@ -12,4 +25,6 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %s, but want %s", got, want)
 	}
+	slowServer.Close()
+	fastSever.Close()
 }
