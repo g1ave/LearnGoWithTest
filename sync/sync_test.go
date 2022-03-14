2bf2e9ec70e6ddaa66319597e4b973006da23f9f
g1ave <qw112657@gmail.com>
2022-03-14 14:58:49 +0800

    Learning Sync

---
+++ sync/sync_test.go
@@ -0,0 +1,43 @@
+package sync
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+		assertCounter(t, counter.Value(), 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter.Value(), wantedCount)
+
+	})
+}
+
+func assertCounter(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d, but want %d", got, want)
+	}
+
+}
