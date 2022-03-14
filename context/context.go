8f0d35a6a5313238fbf3cf7eaf3febbd6d6432c7
g1ave <qw112657@gmail.com>
2022-03-14 14:57:57 +0800

    Learning Context

---
+++ context/context.go
@@ -0,0 +1,69 @@
+package context
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+	t         *testing.T
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancelled")
+	}
+}
+
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Error("store was told to cancelled")
+	}
+}
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
+	}
+}
---
+++ context/go.mod
@@ -0,0 +1,3 @@
+module github.com/g1ave/context
+
+go 1.16
